5ed67a50ff148e719f421b7d4ba4d72bfa92eacf
31000s <burakyabgu@gmail.com>
2023-07-16 22:39:13 +0300

    feat: Add light/dark theme toggle button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Dark from '../assets/dark.svg'
+import Light from '../assets/light.svg'
+
+
 
 const Navbar : React.FC = ()=>{
+
+    const [theme, setTheme] = useState<string>(localStorage.getItem('theme') || 'light')
+
+    useEffect(()=>{
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
     return(
         <nav>
             <ul>
@@ -8,6 +20,10 @@ const Navbar : React.FC = ()=>{
                 <li><a href="#">About</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Contact</a></li>
+
+                <li onClick={()=> setTheme(theme==='light'?'dark' : 'light')} className="themeMode">
+                    {theme==='light' ? <img src={Light} /> : <img src={Dark} />}
+                </li>
             </ul>
         </nav>
     )
