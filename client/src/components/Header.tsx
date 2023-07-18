2bd4f707bd602d411ccab29686514d330985f7b4
31000s <burakyabgu@gmail.com>
2023-07-19 02:31:47 +0300

    feat: Add button to Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HumanVector from '../assets/human-vector.svg'
-
+import {Button} from '../styled/index'
 const Header : React.FC = () =>{
     return(
         <section id="header">
@@ -9,10 +9,13 @@ const Header : React.FC = () =>{
                     <h1>Hi, I am Burak</h1>
                     <h2>I'm Designer</h2>
                     <h3>I am a modern website and Javascript developer. At the same time; Blogger and Designer.</h3>
+                    <Button lg>Hire Now</Button>
                 </div>
                 <div className="header-image-box">
                     <img src={HumanVector} alt="Developer Man" />
                 </div>
+
+
             </div>
         </section>
     )
--- client/src/styled/index.tsx
+++ client/src/styled/index.tsx
@@ -21,4 +21,26 @@ z-index: 1;
 }
 `
 
+interface colorTypes {
+    purple?: any;
+    lg?:any;
+    bold?:any;
+}
+
+export const Button = styled.button<colorTypes>`
+    //props: purple, lg , bold
+    background-color: ${(props) => props.purple ? "#6c55e0" : "#333"};
+    color: #f1ecec;
+    border-radius: 6px;
+    cursor: pointer;
+    padding: ${(props) => props.lg ? ".5rem 2rem;" : ".3rem 1rem"};
+    font-size: ${(props) => props.lg ? "1.1rem" : ".9rem"};
+    font-weight: ${(props) => props.bold ? "500" : "400"};
+
+    &:hover{
+        box-shadow: 0 5px 35px 0px rgb(0 0 0 / 13%);
+        background-color: ${(props) => props.purple ? "#715bdf" : "#292828"};
+    }
+`
+
 
